e129b798e89bc2aead9a261e0714ee86904f21dd
frubbs <rafaelhss@gmail.com>
2016-08-09 23:31:40 -0300

    retirando create ddl

--- src/main/java/downloader/batch/NoticiaWriter.java
+++ src/main/java/downloader/batch/NoticiaWriter.java
@@ -22,7 +22,7 @@ public class NoticiaWriter implements ItemWriter<Noticia> {
         for (Noticia n : list) {
             List<Noticia> noticias = noticiaRepository.findByTitulo(n.getTitulo());
             if (noticias == null || noticias.size() <= 0) {
-                //EmailDispatcher.SendSimpleMessage("rafaelhss@gmail.com", n);
+                EmailDispatcher.SendSimpleMessage("rafaelhss@gmail.com", n);
                 //EmailDispatcher.SendSimpleMessage("humberto.sales@gmail.com", n);
                 noticiaRepository.save(n);
             }
